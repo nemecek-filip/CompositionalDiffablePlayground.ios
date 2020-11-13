e90d95ee7f27aae3a1e4dae624320c73656ceff6
Filip Němeček <nemecek.filip@outlook.com>
2020-11-13 22:29:10 +0100

    Minor changesL

---
+++ CompositionalDiffablePlayground/CompositionalDiffablePlayground/Views/ColorCell.swift
@@ -0,0 +1,23 @@
+//
+//  ColorCell.swift
+//  CompositionalDiffablePlayground
+//
+//  Created by Filip Němeček on 13/11/2020.
+//
+
+import UIKit
+
+class ColorCell: UICollectionViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        contentView.layer.cornerRadius = 10
+    }
+    
+    override var isHighlighted: Bool {
+        didSet {
+            contentView.alpha = isHighlighted ? 0.7 : 1.0
+        }
+    }
+}
